53a94180d44cf41cd864d5d77d73aca1963d8753
Magnum Costa <45879252+magnumcosta@users.noreply.github.com>
2024-02-24 18:57:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,19 @@ consultarItemMaterial_base_url = 'https://dadosabertos.compras.gov.br/modulo-pes
 consultarItemServico_base_url = 'https://dadosabertos.compras.gov.br/modulo-pesquisa-preco/3_consultarServico'
 
 def obter_itens(tipo_item, codigo_item_catalogo=''):
-    if tipo_item == 'Material':
-        response = requests.get(f"{consultarItemMaterial_base_url}?pagina=1&tamanhoPagina=500&codigoItemCatalogo={codigo_item_catalogo}")
-    elif tipo_item == 'Serviço':
-        response = requests.get(f"{consultarItemServico_base_url}?pagina=1&codigoItemCatalogo={codigo_item_catalogo}")
-    else:
-        return []
+    url = consultarItemMaterial_base_url if tipo_item == 'Material' else consultarItemServico_base_url
+    params = {
+        'pagina': 1,
+        'tamanhoPagina': 500
+    }
+    if codigo_item_catalogo:
+        params['codigoItemCatalogo'] = codigo_item_catalogo
 
+    response = requests.get(url, params=params)
     if response.status_code == 200:
-        itens = response.json().get('resultado', [])
-        return [(item['codigoItem'], f"{item['descricao']} (código: {item['codigoItem']})") for item in itens]
+        json_response = response.json()
+        itens = json_response.get('resultado', [])
+        return [(item['codigoItem'], f"{item.get('descricao', 'Descrição não disponível')} (código: {item['codigoItem']})") for item in itens]
     else:
         return []
 
@@ -24,14 +27,13 @@ def obter_itens(tipo_item, codigo_item_catalogo=''):
 st.title("Consulta de Itens de Material e Serviço")
 
 tipo_item = st.selectbox("Selecione o tipo de item para consulta", ['Material', 'Serviço'], key='tipo_item')
-
 codigo_item_catalogo = st.text_input("Código do Item de Catálogo (opcional)", key='codigo_item_catalogo')
 
 if st.button('Consultar', key='btn_consultar'):
     itens = obter_itens(tipo_item, codigo_item_catalogo)
     if itens:
-        for item in itens:
-            st.write(item[1])
+        for codigo, descricao in itens:
+            st.write(descricao)
     else:
         st.error("Nenhum item encontrado ou erro na consulta.")
 
