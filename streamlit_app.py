dd148c7fd39de2653f55b4c20327ef2c9e39d07a
Magnum Costa <45879252+magnumcosta@users.noreply.github.com>
2024-02-24 19:07:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,34 @@
 import streamlit as st
 import requests
-import pandas as pd
 
 # URLs atualizadas
 consultarItemMaterial_base_url = 'https://dadosabertos.compras.gov.br/modulo-pesquisa-preco/1_consultarMaterial'
 consultarItemServico_base_url = 'https://dadosabertos.compras.gov.br/modulo-pesquisa-preco/3_consultarServico'
 
-def obter_itens(tipo_item, codigo_item_catalogo=''):
+def obter_itens(tipo_item, codigo_item_catalogo):
     url = consultarItemMaterial_base_url if tipo_item == 'Material' else consultarItemServico_base_url
     params = {
         'pagina': 1,
-        'tamanhoPagina': 500
+        'tamanhoPagina': 500,
+        'codigoItemCatalogo': codigo_item_catalogo
     }
-    if codigo_item_catalogo:
-        params['codigoItemCatalogo'] = codigo_item_catalogo
 
     response = requests.get(url, params=params)
     if response.status_code == 200:
         json_response = response.json()
+        st.write("Estrutura da Resposta:", json_response)  # Diagnóstico: Imprime a estrutura do JSON
         itens = json_response.get('resultado', [])
-        itens_formatados = []
-        for item in itens:
-            if 'codigoItem' in item and 'descricao' in item:
-                codigo = item['codigoItem']
-                descricao = item.get('descricao', 'Descrição não disponível')
-                itens_formatados.append((codigo, f"{descricao} (código: {codigo})"))
-        return itens_formatados
+        return [(item.get('codigoItem', 'Código não disponível'), 
+                 f"{item.get('descricao', 'Descrição não disponível')} (código: {item.get('codigoItem', 'n/a')})") for item in itens]
     else:
+        st.error(f"Erro na consulta: {response.status_code}")
         return []
 
 # Streamlit UI
 st.title("Consulta de Itens de Material e Serviço")
 
 tipo_item = st.selectbox("Selecione o tipo de item para consulta", ['Material', 'Serviço'], key='tipo_item')
-codigo_item_catalogo = st.text_input("Código do Item de Catálogo (opcional)", key='codigo_item_catalogo')
+codigo_item_catalogo = st.text_input("Código do Item de Catálogo", value="267666", key='codigo_item_catalogo')
 
 if st.button('Consultar', key='btn_consultar'):
     itens = obter_itens(tipo_item, codigo_item_catalogo)
