8419139906d066d52e950dde44e8adbf13f41969
Magnum Costa <45879252+magnumcosta@users.noreply.github.com>
2024-03-20 10:32:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import requests
 
-
 # Função para formatar preço em reais
 def formatar_preco_reais(valor):
     if valor is None:
         return 'Preço não disponível'
     else:
         return f'R$ {valor:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.')
-
 
 # URLs atualizadas
 consultarItemMaterial_base_url = 'https://dadosabertos.compras.gov.br/modulo-pesquisa-preco/1_consultarMaterial'
@@ -18,22 +16,25 @@ def obter_itens(tipo_item, codigo_item_catalogo, pagina):
     url = consultarItemMaterial_base_url if tipo_item == 'Material' else consultarItemServico_base_url
     params = {
         'pagina': pagina,
-        'tamanhoPagina': 10,  # Modificado para 10 registros por página
+        'tamanhoPagina': 10,
         'codigoItemCatalogo': codigo_item_catalogo
     }
-
     response = requests.get(url, params=params)
     if response.status_code == 200:
         json_response = response.json()
         itens = json_response.get('resultado', [])
-        return itens, json_response.get('totalRegistros', 0)  # Retornar também o total de registros
+        return itens, json_response.get('totalRegistros', 0)
     else:
         st.error(f"Erro na consulta: {response.status_code}")
         return [], 0
 
-# Inicialização das variáveis de sessão para controle da paginação
+# Inicialização das variáveis de sessão para controle da paginação e dos dados
 if 'pagina_atual' not in st.session_state:
     st.session_state['pagina_atual'] = 1
+if 'itens' not in st.session_state:
+    st.session_state['itens'] = []
+if 'total_registros' not in st.session_state:
+    st.session_state['total_registros'] = 0
 
 # Streamlit UI
 st.title("Consulta de Itens de Material e Serviço")
@@ -41,35 +42,26 @@ st.title("Consulta de Itens de Material e Serviço")
 tipo_item = st.selectbox("Selecione o tipo de item para consulta", ['Material', 'Serviço'], key='tipo_item')
 codigo_item_catalogo = st.text_input("Código do Item de Catálogo", value="267666", key='codigo_item_catalogo')
 
+if st.button('Consultar'):
+    st.session_state['pagina_atual'] = 1
+    st.session_state['itens'], st.session_state['total_registros'] = obter_itens(tipo_item, codigo_item_catalogo, st.session_state['pagina_atual'])
 
-    
-if st.button('Consultar'):    
-    itens, total_registros = obter_itens(tipo_item, codigo_item_catalogo, 1)
-    if itens:
-        st.write(f"Total de registros encontrados: {total_registros}")
-
-pagina_atual = st.session_state['pagina_atual']
-itens, total_registros = obter_itens(tipo_item, codigo_item_catalogo, pagina_atual)
-
-if itens:
+if st.session_state['itens']:
+    st.write(f"Total de registros encontrados: {st.session_state['total_registros']}")
     # Mostrar os itens em formato de tabela
     tabela_itens = [{
         "Código": item.get('codigoItemCatalogo', 'Código não disponível'), 
         "Descrição": item.get('descricaoItem', 'Descrição não disponível'), 
         "Preço Unit.": formatar_preco_reais(item.get('precoUnitario')),
         "Data do resultado": item.get('dataResultado')
-    } for item in itens]
+    } for item in st.session_state['itens']]
     st.table(tabela_itens)
-    
+
     # Paginação
     if st.button('Anterior', key='btn_anterior'):
         if st.session_state['pagina_atual'] > 1:
             st.session_state['pagina_atual'] -= 1
+            st.session_state['itens'], st.session_state['total_registros'] = obter_itens(tipo_item, codigo_item_catalogo, st.session_state['pagina_atual'])
     if st.button('Próximo', key='btn_proximo'):
-        if pagina_atual * 10 < total_registros:
-            st.session_state['pagina_atual'] += 1
-else:
-    st.error("Nenhum item encontrado ou erro na consulta.")
-
-
-
+        if st.session_state['pagina_atual'] * 10 < st.session_state['total_registros']:
+            st.session
